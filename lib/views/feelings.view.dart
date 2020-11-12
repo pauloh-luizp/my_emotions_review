ac61d099d06bc9d59c9ff2702fe81e26cf07e4ad
x0Henr1Must47d <paulo0147wp@outlook.com>
2020-11-12 10:52:09 -0300

    0.035

--- lib/views/feelings.view.dart
+++ lib/views/feelings.view.dart
@@ -8,6 +8,8 @@ class Feelings extends StatefulWidget {
 }
 
 class _FeelingsState extends State<Feelings> {
+  String currentFeeling;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +17,72 @@ class _FeelingsState extends State<Feelings> {
         title: Text('My Emotions Review'),
         centerTitle: true,
         backgroundColor: Colors.orange[800],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            RadioListTile(
+              title: Text("++ Feliz"),
+              subtitle: Text("Fico muito feliz pois você está muito feliz!"),
+              activeColor: Colors.lightGreenAccent[400],
+              value: "Muito Feliz",
+              groupValue: currentFeeling,
+              onChanged: (String valor) {
+                setState(() {
+                  currentFeeling = valor;
+                });
+              },
+            ),
+            RadioListTile(
+              title: Text("+ Feliz"),
+              subtitle: Text("Eeee que bom que está feliz!"),
+              activeColor: Colors.lightGreen,
+              value: "Feliz",
+              groupValue: currentFeeling,
+              onChanged: (String valor) {
+                setState(() {
+                  currentFeeling = valor;
+                });
+              },
+            ),
+            RadioListTile(
+              title: Text("= Neutro"),
+              subtitle: Text("Me diga como se sente"),
+              activeColor: Colors.blueGrey,
+              value: "Neutro",
+              groupValue: currentFeeling,
+              onChanged: (String valor) {
+                setState(() {
+                  currentFeeling = valor;
+                });
+              },
+            ),
+            RadioListTile(
+              title: Text("- Triste"),
+              subtitle: Text("Quer conversar?"),
+              activeColor: Colors.red[700],
+              value: "Triste",
+              groupValue: currentFeeling,
+              onChanged: (String valor) {
+                setState(() {
+                  currentFeeling = valor;
+                });
+              },
+            ),
+            RadioListTile(
+              title: Text("-- Triste"),
+              subtitle: Text("Não fique assim, vamos conversar"),
+              activeColor: Colors.redAccent[700],
+              value: "Muito Triste",
+              groupValue: currentFeeling,
+              onChanged: (String valor) {
+                setState(() {
+                  currentFeeling = valor;
+                });
+              },
+            )
+          ],
+        ),
       ),
     );
   }
